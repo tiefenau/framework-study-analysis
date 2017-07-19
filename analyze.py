60824f9ac049dd4fe01e1ec8da456e37a0e6abe1
chris <tiefenau@cs.uni-bonn.de>
2017-07-19 14:48:24 +0200

    improved output

--- analyze.py
+++ analyze.py
@@ -9,15 +9,19 @@ data_jsf = data_all[data_all['Framework'] == 'JSF']
 data_jsf = data_all[data_all['Framework'] == 'Spring']
 
 testset = [
-    {'check' : 'fwsupport ~ C(Framework)' ,  'set':data_primed},
-    {'check' : 'fwsupport ~ C(Framework)' ,  'set':data_all},
-    {'check' : 'Security ~ secscore' ,  'set':data_primed},
-    {'check' : 'Security ~ secscore' ,  'set':data_all},
-    {'check' : 'Functional ~ C(Q22)' ,  'set':data_all},
-    {'check' : 'tasktime ~ C(Q22)' ,  'set':data_all},
+    {'check' : 'fwsupport ~ C(Framework)' ,  'set':data_primed, 'text':'primed_only'},
+    {'check' : 'fwsupport ~ C(Framework)' ,  'set':data_all, 'text':'all'},
+    {'check' : 'Security ~ secscore' ,  'set':data_primed, 'text':'primed_only'},
+    {'check' : 'Security ~ secscore' ,  'set':data_all, 'text':'all'},
+    #{'check' : 'C(Functional) ~ C(Q22)' ,  'set':data_all},
+    #{'check' : 'tasktime ~ C(Q22)' ,  'set':data_all},
 ]
 
 for test in testset:
     r = ols(test['check'],test['set']).fit()
-    print test['check'] + " : "
-    print "r_sq = " + str(r.rsquared)
+    print test['check'] + " ("+test['text']+") : "
+    print "r_sq       = " + str(r.rsquared)
+    print "p          = " + str(r.f_pvalue)
+    print "beta_coeffs: "
+    print r.params
+    print ""
